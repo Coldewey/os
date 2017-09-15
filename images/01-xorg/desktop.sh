1aff85667e27ec88470f68c5d9a9cc0014444aac
Phil Lehmkuhl <plehmkuhl@coldewey.de>
2017-09-15 10:41:57 +0200

    Implements xorg and graphical boot with grub

--- images/01-xorg/desktop.sh
+++ images/01-xorg/desktop.sh
@@ -2,4 +2,8 @@
 
 rm -f /tmp/.X0-lock
 
-exec /usr/bin/Xorg :0 -auth "$XSERVER_AUTHFILE" -nolisten tcp $DISP_VT
+if [[ -n `lsusb | grep 0eef` ]]; then
+    NOCURSOR="-nocursor"
+fi
+
+exec /usr/bin/Xorg :0 -auth "$XSERVER_AUTHFILE" $NOCURSOR -nolisten tcp $DISP_VT
